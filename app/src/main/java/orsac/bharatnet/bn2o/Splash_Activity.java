bd82b0154e9f13a85ad56c69cb76a2b775469621
madhudashore <dashore.mad@gmail.com>
2018-09-27 15:53:37 +0530

    version 1.0

---
+++ app/src/main/java/orsac/bharatnet/bn2o/Splash_Activity.java
@@ -0,0 +1,12 @@
+package orsac.bharatnet.bn2o;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_);
+    }
+}
